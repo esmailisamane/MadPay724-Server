e23163261a582ab861b2039a2d3c9c07a509b7c4
esmailisamane <s.esmaili30@gmail.com>
2020-03-13 12:53:55 +0330

    data

---
+++ MadPay724.Data/DatabaseContext/MadpayDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay724.Data.DatabaseContext
+{
+    class MadpayDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-HO9R1KR\SA ;Initial Catalog = MadPay724db; Integrated Security= True; MultipledActiveResultSets= True");
+        }
+    }
+}
