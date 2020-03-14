9cfd930397bcb26fcea1126d21587f30f2ea63d8
esmailisamane <s.esmaili30@gmail.com>
2020-03-14 20:05:07 +0330

    injection

--- MadPay724.Data/Infrastructure/IUnitOfWork.cs
+++ MadPay724.Data/Infrastructure/IUnitOfWork.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using MadPay724.Data.Repositories.Interface;
+using MadPay724.Data.Repositories.repo;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MadPay724.Data.Infrastructure
 {
-  public  interface IUnitOfWork
+  public  interface IUnitOfWork<TContext> :IDisposable where TContext:DbContext
     {
+        IUserRepository UserRepository { get;}
+        void Save();
+        Task<int> saveAsync();
     }
 }
--- MadPay724.Data/Infrastructure/UnitOfWork.cs
+++ MadPay724.Data/Infrastructure/UnitOfWork.cs
@@ -1,10 +1,70 @@
-﻿using System;
+﻿using MadPay724.Data.Repositories.Interface;
+using MadPay724.Data.Repositories.repo;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MadPay724.Data.Infrastructure
 {
-  public  class UnitOfWork
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext> where TContext : DbContext, new()
     {
+        protected readonly DbContext _db;
+        public UnitOfWork()
+        {
+            _db = new TContext();
+        }
+
+        private bool disposed = false;
+
+
+        private IUserRepository userRepository;
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                if (userRepository == null)
+                {
+                    userRepository = new UserRepository(_db);
+                }
+                return userRepository;
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _db.Dispose();
+                }
+               
+            }
+            disposed = true;
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public Task<int> saveAsync()
+        {
+            return _db.SaveChangesAsync();
+        }
+
+        ~UnitOfWork()
+        {
+            Dispose(false);
+        }
     }
 }
