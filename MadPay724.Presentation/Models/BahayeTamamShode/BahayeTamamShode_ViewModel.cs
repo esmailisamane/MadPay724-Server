b6769419376f729a86f7813980920260736c49ea
esmailisamane <s.esmaili30@gmail.com>
2020-07-01 16:49:00 +0430

    .

---
+++ MadPay724.Presentation/Models/BahayeTamamShode/BahayeTamamShode_ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MadPay724.Presentation.Models.BahayeTamamShode
+{
+    public class BahayeTamamShode_ViewModel
+    {
+        public int Code { get; set; }
+        public string Title { get; set; }
+        public string BetonC25 { get; set; }
+        public string BetonC30 { get; set; }
+        public string BetonC35 { get; set; }
+        public string BetonC40 { get; set; }
+        public string SumRow { get; set; }
+        public string Description { get; set; }
+    }
+}
